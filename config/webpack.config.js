0f67bccd36ab74242dc46252b6ca278c2615b21a
cytruslabs <cytruslabs@github.com>
2022-03-18 20:24:32 +0000

    Webpack added + WIP Transactions

---
+++ config/webpack.config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common.js');
+const PATHS = require('./paths');
+
+// Merge webpack configuration files
+const config = (env, argv) => merge(common, {
+  entry: {
+    popup: PATHS.src + '/popup.js',
+    contentScript: PATHS.src + '/contentScript.js',
+    background: PATHS.src + '/background.js',
+    notif: PATHS.src + '/notif.js',
+    tx: PATHS.src + '/tx.js'
+  },
+  devtool: argv.mode === 'production' ? false : 'source-map'
+});
+
+module.exports = config;
