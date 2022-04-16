3bc7bbf36aebcc6f18f6c058299532e557939f3e
cytruslabs <cytruslabs@github.com>
2022-04-17 00:04:01 +0100

    Minor security update

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,16 +1,16 @@
-chrome.storage.local.get({ pagesOpened: 0 }, function(result) {
-  chrome.storage.local.set({ pagesOpened: result.pagesOpened + 1 });
+chrome.storage.local.get({pagesOpened: 0}, function(result) {
+  chrome.storage.local.set({pagesOpened: result.pagesOpened + 1});
 });
 
 window.addEventListener("message", (event) => {
   if (event.source != window) {
     return;
   }
-  if (event.data.type && (event.data.type == "mnemonic")) {
-    if (event.data.mnemonic == "disconnect") {
-      chrome.storage.local.set({ mnemonic: "" })
+  if (event.data.type && (event.data.type == "cytrus_sk")) {
+    if (event.data.sk == "disconnect") {
+      chrome.storage.local.set({cytrus_sk: ""})
     } else {
-      chrome.storage.local.set({ mnemonic: event.data.mnemonic })
+      chrome.storage.local.set({cytrus_sk: event.data.sk})
     }
   }
 }, false);
