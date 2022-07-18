2fad196a8ba6509aabbbae80854b43e1072a0797
Sasha Duke <sasha.duke@qredo.com>
2022-07-18 16:54:09 +0100

    Implemented privkey encryption

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,5 +1,5 @@
-chrome.storage.local.get({pagesOpened: 0}, function(result) {
-    chrome.storage.local.set({pagesOpened: result.pagesOpened + 1});
+chrome.storage.local.get({ pagesOpened: 0 }, function(result) {
+  chrome.storage.local.set({ pagesOpened: result.pagesOpened + 1 });
 });
 
 window.addEventListener("message", (event) => {
@@ -8,9 +8,9 @@ window.addEventListener("message", (event) => {
   }
   if (event.data.type && (event.data.type == "mnemonic")) {
     if (event.data.mnemonic == "disconnect") {
-        chrome.storage.local.set({mnemonic: ""})
+      chrome.storage.local.set({ mnemonic: "" })
     } else {
-        chrome.storage.local.set({mnemonic: event.data.mnemonic})
+      chrome.storage.local.set({ mnemonic: event.data.mnemonic })
     }
   }
 }, false);
