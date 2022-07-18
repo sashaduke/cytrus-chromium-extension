cd3c7b45c96825827fc6100689bae70613011a1e
Sasha Duke <sasha.duke@qredo.com>
2022-07-18 16:54:05 +0100

    Implemented API

--- src/notif.js
+++ src/notif.js
@@ -1,39 +1,19 @@
-//import { Writer } from "protobufjs/minimal";
 import { SigningStargateClient } from "@cosmjs/stargate";
-import { Registry } from "@cosmjs/proto-signing";
-import { MsgPromoClicked } from "./tx.js";
-import { MsgPromoViewed } from "./tx.js";
-import { MsgCreatePromo } from "./tx.js";
+import { Registry, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { MsgPromoClicked, MsgPromoViewed, MsgCreatePromo } from "./tx.js";
 
 const types = [
     ["/cytruslabs.zestchain.zestchain.MsgPromoClicked", MsgPromoClicked],
     ["/cytruslabs.zestchain.zestchain.MsgPromoViewed", MsgPromoViewed],
     ["/cytruslabs.zestchain.zestchain.MsgCreatePromo", MsgCreatePromo],
 ];
-const MissingWalletError = new Error("wallet is required");
 const registry = new Registry(types);
-const defaultFee = {
+const fee = {
     amount: [],
     gas: "200000",
 };
-const txClient = async (wallet, { addr: addr } = { addr: "http://localhost:26657" }) => {
-    if (!wallet)
-        throw MissingWalletError;
-    let client;
-    if (addr) {
-        client = await SigningStargateClient.connectWithSigner(addr, wallet, { registry });
-    }
-    else {
-        client = await SigningStargateClient.offline(wallet, { registry });
-    }
-    const { address } = (await wallet.getAccounts())[0];
-    return {
-        signAndBroadcast: (msgs, { fee, memo } = { fee: defaultFee, memo: "" }) => client.signAndBroadcast(address, msgs, fee, memo),
-        msgPromoClicked: (data) => ({ typeUrl: "/cytruslabs.zestchain.zestchain.MsgPromoClicked", value: data}),//MsgPromoClicked.fromPartial(data) }),
-        msgPromoViewed: (data) => ({ typeUrl: "/cytruslabs.zestchain.zestchain.MsgPromoViewed", value: data}),//MsgPromoViewed.fromPartial(data) }),
-        msgCreatePromo: (data) => ({ typeUrl: "/cytruslabs.zestchain.zestchain.MsgCreatePromo", value: data})//MsgCreatePromo.fromPartial(data) }),
-    };
-};
+const mnemonic = "choose crack spoon large rally divide eagle cube magnet dial common angle welcome enact shadow hand hockey cost stem sunny fabric thank grief region";
+const rpcEndpoint = "http://zestcha.in:26657";
 
 let notif = {
     type: 'basic',
@@ -48,42 +28,25 @@ notif.message = 'Zero trading fees for 3 months when you sign up today!';
 let notifURL = 'https://binance.com';
 let id = Math.random().toString();
 
-/*
-let encode = function(message, writer = Writer.create()) {
-    if (message.creator !== "") {
-        writer.uint32(10).string(message.creator);
-    }
-    if (message.title !== "") {
-        writer.uint32(18).string(message.title);
-    }
-    if (message.pot !== 0) {
-        writer.uint32(24).uint64(message.pot);
-    }
-    if (message.url !== "") {
-        writer.uint32(34).string(message.url);
-    }
-    if (message.msg !== "") {
-        writer.uint32(42).string(message.msg);
-    }
-    if (message.tags !== "") {
-        writer.uint32(50).string(message.tags);
-    }
-    if (message.prefs !== "") {
-        writer.uint32(58).string(message.prefs);
-    }
-    return writer.finish();
-};*/
-
 function notifClicked() {
     chrome.tabs.create({url: notifURL});
     chrome.notifications.clear(id);
 };
 chrome.notifications.onClicked.addListener(notifClicked);
 
-async function sendMsgPromoViewed(data) {
-    const message = await txClient.msgCreatePromo(data);
-    const memo = '';
-    const result = await txClient.signAndBroadcast([message], {fee: {amount: [], gas: "200000"}, memo});
+async function sendMsgCreatePromo(promo) {
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic);
+    const [shulgin] = await wallet.getAccounts();
+    const client = await SigningStargateClient.connectWithSigner(
+        rpcEndpoint,
+        wallet,
+        { registry: registry }
+    );
+    const msg = {
+        typeUrl: types[2][0],
+        value: promo
+    };
+    const result = await client.signAndBroadcast(shulgin.address, [msg], fee);
     console.log(result);
 };
 
@@ -96,7 +59,7 @@ function submitForm(event) {
     let tags = document.getElementById('tags').value;
     let prefs = document.getElementById('prefs').value;
    
-    let promoMsg = {
+    let promo = {
         creator: creator,
         title: title,
         pot: pot,
@@ -107,12 +70,7 @@ function submitForm(event) {
     };
 
     event.preventDefault();
-    let data = MsgCreatePromo.encode(promoMsg).finish();
-    console.log(data.toString());
-    sendMsgPromoViewed(data);
-    sendMsgPromoViewed(promoMsg);
-    //const promise = this.rpc.request("cytruslabs.zestchain.zestchain.Msg", "CreatePromo", data);
-    //console.log(promise.then((data) => MsgCreatePromoResponse.decode(new Reader(data))));
+    sendMsgCreatePromo(promo);
 };
 
 chrome.notifications.create(id, notif);
